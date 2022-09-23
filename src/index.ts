1aaaac0472b6414577412a8e9ee2891affb1de7e
IvanHGo <ivan.gobbi@radiumrocket.com>
2022-09-23 12:18:55 -0300

    QL-21: mongoDB connection, eslint error correction

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,13 @@ dotenv.config();
 const port = process.env.PORT;
 const URL = process.env.DATABASE_URL || '';
 
-mongoose.connect(
-  URL,
-  (error) => {
-    if (error) {
-      console.log('Fail to connect', error);
-    } else {
-      app.listen(port, () => {
-        console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
-      });
-      console.log('Connected to database');
-    }
-  },
-);
+mongoose.connect(URL, (error) => {
+  if (error) {
+    console.log('Fail to connect', error);
+  } else {
+    app.listen(port, () => {
+      console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
+    });
+    console.log('Connected to database');
+  }
+});
