de35690d76f31b7109b62f33ce403159d08e2529
IvanHGo <ivan.gobbi@radiumrocket.com>
2022-09-27 17:17:31 -0300

    QL-46: create/update/delete a category functions

--- src/routes/category/controllers.ts
+++ src/routes/category/controllers.ts
@@ -44,7 +44,82 @@ const getCategoryById = async (req: Request, res: Response) => {
   }
 };
 
+const createCategory = async (req: Request, res: Response) => {
+  try {
+    const category = new Category({
+      name: req.body.name,
+      image: req.body.image,
+      isActive: req.body.isActive,
+      logicDelete: req.body.logicDelete,
+    });
+    const result = await category.save();
+    return res.status(201).json({
+      message: 'Category created successfully',
+      data: result,
+      error: false,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      message: error.message,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const updateCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryUpdate = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!categoryUpdate) {
+      return res.status(404).json({
+        message: `Could not found a category by the id of ${req.params.id}.`,
+        data: undefined,
+        error: true,
+      });
+    }
+    return res.status(200).json({
+      message: 'Category updated successfully',
+      data: categoryUpdate,
+      error: false,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: 'There was an error with the request',
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
+const deleteCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryDelete = await Category.findByIdAndRemove(req.params.id);
+
+    if (!categoryDelete) {
+      return res.status(404).json({
+        message: `Could not found a category by the id of ${req.params.id}.`,
+        data: undefined,
+        error: true,
+      });
+    }
+    return res.status(200).json({
+      message: 'Category deleted successfully',
+      data: categoryDelete,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      message: 'There was an error with the request',
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
 export default {
   getAllCategories,
   getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
 };
--- src/routes/category/index.ts
+++ src/routes/category/index.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.get('/', controllers.getAllCategories);
 router.get('/:id', controllers.getCategoryById);
+router.post('/', controllers.createCategory);
+router.patch('/:id', controllers.updateCategory);
+router.delete('/:id', controllers.deleteCategory);
 
 export default router;
