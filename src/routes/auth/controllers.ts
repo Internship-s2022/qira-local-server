8cbbc14b140fcbd9e08be367446542b0bdaa992a
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-10-14 17:47:15 -0300

    QL-91: fixed get auth user function

--- src/routes/auth/controllers.ts
+++ src/routes/auth/controllers.ts
@@ -1,32 +1,49 @@
 import { Response } from 'express';
 
-import { RequestWithFirebase } from 'src/interfaces';
+import firebase from 'src/helper/firebase';
+import { RequestWithFirebase, Role } from 'src/interfaces';
 import Admin from 'src/models/admin';
 import Client from 'src/models/client';
 
 export const getUser = async (req: RequestWithFirebase, res: Response) => {
+  const { token } = req.headers;
+  if (!token || typeof token !== 'string') {
+    return res.status(400).json({ message: 'Token is required' });
+  }
   try {
-    const admin = await Admin.findOne({
-      firebaseUid: req.firebaseUid,
-      logicDelete: false,
-    });
-    if (admin) {
-      return res.status(200).json({
-        message: 'Showing Admin Data',
-        data: admin,
-        error: false,
+    const response = await firebase.auth().verifyIdToken(token);
+    if (!response.role) {
+      return res.status(403).json({
+        message: 'No credentials found',
+        data: undefined,
+        error: true,
       });
     }
-    const client = await Client.findOne({
-      firebaseUid: req.firebaseUid,
-      logicDelete: false,
-    });
-    if (client) {
-      return res.status(200).json({
-        message: 'Showing Client Data',
-        data: client,
-        error: false,
+    if (response.role === Role.ADMIN) {
+      const admin = await Admin.findOne({
+        firebaseUid: req.firebaseUid,
+        logicDelete: false,
       });
+      if (admin) {
+        return res.status(200).json({
+          message: 'Showing Admin Data',
+          data: admin,
+          error: false,
+        });
+      }
+    }
+    if (response.role === Role.CLIENT) {
+      const client = await Client.findOne({
+        firebaseUid: req.firebaseUid,
+        logicDelete: false,
+      });
+      if (client) {
+        return res.status(200).json({
+          message: 'Showing Client Data',
+          data: client,
+          error: false,
+        });
+      }
     }
     return res.status(404).json({
       message: 'User not found',
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 
-import { Role } from 'src/interfaces';
-import { authMiddleware } from 'src/middlewares/firebase';
-
 import * as controllers from './controllers';
 
 const router = express.Router();
 
-router.use('/data', authMiddleware(Role.CLIENT || Role.ADMIN), controllers.getUser);
+router.get('/user', controllers.getUser);
 
 export default router;
