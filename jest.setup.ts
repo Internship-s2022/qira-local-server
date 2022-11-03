597d513d20f1b87f94a5626555bf736e36a6d3f4
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-03 16:22:57 -0300

    QL-251: fixed firebase error

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,5 @@
-import { config } from 'dotenv';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-
-config();
 
 const testMongoServer = new MongoMemoryServer();
 let mongo: MongoMemoryServer;
--- src/helper/firebase/index.ts
+++ src/helper/firebase/index.ts
@@ -1,15 +1,14 @@
-import { config } from 'dotenv';
 import admin from 'firebase-admin';
 
-config();
+if (!process.env.IS_TEST) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      privateKey:
+        process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+    }),
+  });
+}
 
-const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert({
-    privateKey:
-      process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  }),
-});
-
-export default firebaseApp;
+export default admin;
