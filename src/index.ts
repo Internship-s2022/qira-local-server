c7d82a5d64b7eba10627d742af194edba7cfbc4b
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-03 15:46:36 -0300

    QL-251: Setup mongodb-memory-server

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,15 @@ import app from './app';
 const port = process.env.PORT;
 const URL = process.env.DATABASE_URL || '';
 
-mongoose.connect(URL, (error) => {
-  if (error) {
-    console.log('Fail to connect', error);
-  } else {
-    app.listen(port, () => {
-      console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
-    });
-    console.log('Connected to database');
-  }
-});
+if (!process.env.IS_TEST) {
+  mongoose.connect(URL, (error) => {
+    if (error) {
+      console.log('Fail to connect', error);
+    } else {
+      app.listen(port, () => {
+        console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
+      });
+      console.log('Connected to database');
+    }
+  });
+}
