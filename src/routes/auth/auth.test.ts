b3fdd0bf603359a6e15b4237fd7c97e5108268be
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-03 15:48:02 -0300

    QL-251: Deleted console.log

--- src/routes/auth/auth.test.ts
+++ src/routes/auth/auth.test.ts
@@ -5,7 +5,6 @@ import app from 'src/app';
 describe('example test', () => {
   test('example test', async () => {
     const response = await request(app).get('/public/categories').send();
-    console.log(response.body);
     expect(response.status).toBe(200);
   });
 });
