f490968d5c8e8647498b4f469909d607a504e921
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-10 14:33:38 -0300

    QL-263: Create Order endpoint

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import categoriesRouter from './categories';
+import orderRouter from './order';
 import productsRouter from './products';
 
 const router = express.Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/products', productsRouter);
+router.use('/order', orderRouter);
 
 export default router;
