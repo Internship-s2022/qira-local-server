5774e7a1928b21a189a7ee9115152879bea01582
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-10 16:24:07 -0300

    QL-263: Create Order endpoint working

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import { Role } from 'src/interfaces';
+import { authMiddleware } from 'src/middlewares/firebase';
+
 import categoriesRouter from './categories';
 import orderRouter from './order';
 import productsRouter from './products';
@@ -8,6 +11,6 @@ const router = express.Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/products', productsRouter);
-router.use('/order', orderRouter);
+router.use('/order', authMiddleware(Role.CLIENT), orderRouter);
 
 export default router;
