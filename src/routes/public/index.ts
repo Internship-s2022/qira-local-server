e2aeb3ade7a4f80a3bab1dc4dc9aa8697d11fbc7
IvanHGo <ivan.gobbi@radiumrocket.com>
2022-11-17 17:07:37 -0300

    QL-192: PR corrections, order endpoints moved from public to client router

--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -1,16 +1,11 @@
 import express from 'express';
 
-import { Role } from 'src/interfaces';
-import { authMiddleware } from 'src/middlewares/firebase';
-
 import categoriesRouter from './categories';
-import ordersRouter from './orders';
 import productsRouter from './products';
 
 const router = express.Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/products', productsRouter);
-router.use('/orders', authMiddleware(Role.CLIENT), ordersRouter);
 
 export default router;
