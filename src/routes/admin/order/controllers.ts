d744afe3cb657caa523dc6925bbffd7d6f448ce6
FranGutierrez <francisco.gutierrez@radiumrocket.com>
2022-11-25 09:21:44 -0300

    QL-195: Populate products in get order by id endpoint

--- src/routes/admin/order/controllers.ts
+++ src/routes/admin/order/controllers.ts
@@ -21,7 +21,9 @@ export const getAllOrders = async (req: Request, res: Response) => {
 
 export const getOrderById = async (req: Request, res: Response) => {
   try {
-    const order = await Order.findOne({ _id: req.params.id }).populate('client');
+    const order = await Order.findOne({ _id: req.params.id })
+      .populate('client')
+      .populate('products.product');
     if (!order) {
       return res.status(404).json({
         message: `Could not find an order by the id of ${req.params.id}.`,
