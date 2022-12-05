b844a4fb623f5271a43c4b43cf798a027cecb28a
IvanHGo <ivan.gobbi@radiumrocket.com>
2022-12-05 16:51:01 -0300

    QL-311: updated missing errors to new format

--- src/routes/admin/order/controllers.ts
+++ src/routes/admin/order/controllers.ts
@@ -30,161 +30,111 @@ export const getOrderById = async (req: Request, res: Response) => {
 };
 
 export const getOrderToDeliver = async (req: Request, res: Response) => {
-  try {
-    const order = await Order.findOne({ _id: req.params.id })
-      .populate('client')
-      .populate('products.product');
-    if (!order) {
-      return res.status(404).json({
-        message: `Could not find an order by the id of ${req.params.id}.`,
-        data: undefined,
-        error: true,
-      });
-    }
-    const validAuthorized = order.authorized.some((authorized) => {
-      return authorized.dni === req.query.dni;
-    });
-    if (!validAuthorized) {
-      return res.status(404).json({
-        message: `Could not find an authorized with the dni ${req.query.dni}.`,
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: `Showing the specified order by the id of ${req.params.id}.`,
-      data: order,
-      error: false,
-    });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
+  const order = await Order.findOne({ _id: req.params.id })
+    .populate('client')
+    .populate('products.product');
+  if (!order) {
+    throw new CustomError(404, `Could not find an order by the id of ${req.params.id}.`);
   }
+  const validAuthorized = order.authorized.some((authorized) => {
+    return authorized.dni === req.query.dni;
+  });
+  if (!validAuthorized) {
+    throw new CustomError(404, `Could not find an authorized with the dni ${req.query.dni}.`);
+  }
+  return res.status(200).json({
+    message: `Showing the specified order by the id of ${req.params.id}.`,
+    data: order,
+    error: false,
+  });
 };
 
 export const approveOrder = async (req: Request, res: Response) => {
-  try {
-    const invoice = req.body.invoice;
-    const uploadInvoice = await s3.uploadFile(invoice, process.env.AWS_BUCKET_ORDER_INVOICE || '');
-    const invoiceFile = {
-      key: uploadInvoice.Key,
-      url: uploadInvoice.Location,
-    };
-    const orderUpdate = await Order.findOneAndUpdate(
-      { _id: req.params.id, logicDelete: false, state: OrderState.APPROVE_PENDING },
-      { state: OrderState.DELIVERY_PENDING, invoice: invoiceFile, payAuthDate: Date.now() },
-      { new: true },
-    )
-      .populate('client')
-      .populate('products.product');
-    if (!orderUpdate) {
-      return res.status(404).json({
-        message: `Could not find an order by the id of ${req.params.id} or the change you are trying to make it is not allowed`,
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: 'Order state updated successfully.',
-      data: orderUpdate,
-      error: false,
-    });
-  } catch (error: any) {
-    return res.status(400).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
+  const invoice = req.body.invoice;
+  const uploadInvoice = await s3.uploadFile(invoice, process.env.AWS_BUCKET_ORDER_INVOICE || '');
+  const invoiceFile = {
+    key: uploadInvoice.Key,
+    url: uploadInvoice.Location,
+  };
+  const orderUpdate = await Order.findOneAndUpdate(
+    { _id: req.params.id, logicDelete: false, state: OrderState.APPROVE_PENDING },
+    { state: OrderState.DELIVERY_PENDING, invoice: invoiceFile, payAuthDate: Date.now() },
+    { new: true },
+  )
+    .populate('client')
+    .populate('products.product');
+  if (!orderUpdate) {
+    throw new CustomError(
+      404,
+      `Could not find an order by the id of ${req.params.id} or the change you are trying to make it is not allowed.`,
+    );
   }
+  return res.status(200).json({
+    message: 'Order state updated successfully.',
+    data: orderUpdate,
+    error: false,
+  });
 };
 
 export const deliverOrder = async (req: Request, res: Response) => {
-  try {
-    const signedInvoice = req.body.signedInvoice;
-    const uploadSignedInvoice = await s3.uploadFile(
-      signedInvoice,
-      process.env.AWS_BUCKET_ORDER_SIGNED_INVOICE || '',
-    );
-    const signedInvoiceFile = {
-      key: uploadSignedInvoice.Key,
-      url: uploadSignedInvoice.Location,
-    };
+  const signedInvoice = req.body.signedInvoice;
+  const uploadSignedInvoice = await s3.uploadFile(
+    signedInvoice,
+    process.env.AWS_BUCKET_ORDER_SIGNED_INVOICE || '',
+  );
+  const signedInvoiceFile = {
+    key: uploadSignedInvoice.Key,
+    url: uploadSignedInvoice.Location,
+  };
 
-    const orderUpdate = await Order.findOneAndUpdate(
-      { _id: req.params.id, logicDelete: false, state: OrderState.DELIVERY_PENDING },
-      { state: OrderState.DELIVERED, signedInvoice: signedInvoiceFile, deliverDate: Date.now() },
-      { new: true },
-    )
-      .populate('client')
-      .populate('products.product');
-    if (!orderUpdate) {
-      return res.status(404).json({
-        message: `Could not find an order by the id of ${req.params.id} or the change you are trying to make it is not allowed`,
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: 'Order state updated successfully.',
-      data: orderUpdate,
-      error: false,
-    });
-  } catch (error: any) {
-    return res.status(400).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
+  const orderUpdate = await Order.findOneAndUpdate(
+    { _id: req.params.id, logicDelete: false, state: OrderState.DELIVERY_PENDING },
+    { state: OrderState.DELIVERED, signedInvoice: signedInvoiceFile, deliverDate: Date.now() },
+    { new: true },
+  )
+    .populate('client')
+    .populate('products.product');
+  if (!orderUpdate) {
+    throw new CustomError(
+      404,
+      `Could not find an order by the id of ${req.params.id} or the change you are trying to make it is not allowed.`,
+    );
   }
+  return res.status(200).json({
+    message: 'Order state updated successfully.',
+    data: orderUpdate,
+    error: false,
+  });
 };
 
 export const rejectOrder = async (req: Request, res: Response) => {
-  try {
-    const order = await Order.findOneAndUpdate(
-      { _id: req.params.id, logicDelete: false, state: OrderState.APPROVE_PENDING },
-      { state: OrderState.REJECTED },
-      { new: true },
-    )
-      .populate('client')
-      .populate('products.product');
-    if (!order) {
-      return res.status(404).json({
-        message: `Could not find an order by the id of ${req.params.id}.`,
-        data: undefined,
-        error: true,
-      });
-    }
-    order.products.forEach(async (product) => {
-      const completeProduct = await Product.findById(product.product);
-      if (completeProduct) {
-        const productUpdate = await Product.findOneAndUpdate(
-          { _id: product.product, logicDelete: false },
-          { stock: completeProduct.stock + product.quantity },
-          { new: true },
-        );
-        if (!productUpdate) {
-          return res.status(400).json({
-            message: 'Something went wrong.',
-            data: undefined,
-            error: true,
-          });
-        }
-      }
-    });
-
-    return res.status(200).json({
-      message: `Order rejected successfully ${req.params.id}.`,
-      data: order,
-      error: false,
-    });
-  } catch (error: any) {
-    return res.status(500).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
+  const order = await Order.findOneAndUpdate(
+    { _id: req.params.id, logicDelete: false, state: OrderState.APPROVE_PENDING },
+    { state: OrderState.REJECTED },
+    { new: true },
+  )
+    .populate('client')
+    .populate('products.product');
+  if (!order) {
+    throw new CustomError(404, `Could not find an order by the id of ${req.params.id}.`);
   }
+  order.products.forEach(async (product) => {
+    const completeProduct = await Product.findById(product.product);
+    if (completeProduct) {
+      const productUpdate = await Product.findOneAndUpdate(
+        { _id: product.product, logicDelete: false },
+        { stock: completeProduct.stock + product.quantity },
+        { new: true },
+      );
+      if (!productUpdate) {
+        throw new CustomError(400, 'Something went wrong.');
+      }
+    }
+  });
+
+  return res.status(200).json({
+    message: `Order rejected successfully ${req.params.id}.`,
+    data: order,
+    error: false,
+  });
 };
--- src/routes/public/exchangeRate/controllers.ts
+++ src/routes/public/exchangeRate/controllers.ts
@@ -1,69 +1,50 @@
 import { Request, Response } from 'express';
 import fetch from 'node-fetch';
 
+import { CustomError } from 'src/middlewares/error-handler/custom-error.model';
 import Settings from 'src/models/settings';
 
 import { ExchangeRate } from './types';
 
 export const getExchangeRate = async (req: Request, res: Response) => {
-  try {
-    const settings = await Settings.find();
-    if (!settings) {
-      return res.status(404).json({
-        message: 'There are no settings.',
-        data: undefined,
-        error: true,
-      });
-    }
-    const lastExchangeRate = settings[0].exchangeRate;
-    const today = new Date();
-    const yesterday = new Date(today.getTime());
-    yesterday.setDate(today.getDate() - 1);
+  const settings = await Settings.find();
+  if (!settings) {
+    throw new CustomError(404, 'There are no settings.');
+  }
+  const lastExchangeRate = settings[0].exchangeRate;
+  const today = new Date();
+  const yesterday = new Date(today.getTime());
+  yesterday.setDate(today.getDate() - 1);
 
-    if (lastExchangeRate.date === yesterday.toISOString().substring(0, 10)) {
-      return res.status(200).json({
-        message: 'Showing exchange rate.',
-        data: lastExchangeRate,
-        error: false,
-      });
-    }
-    const response = await fetch('https://api.estadisticasbcra.com/usd_of', {
-      headers: {
-        Authorization: process.env.TOKEN_BCRA || '',
-      },
-    });
-    const data: ExchangeRate[] = await response.json();
-
-    if (!data) {
-      return res.status(404).json({
-        message: 'Something went wrong ',
-        data: undefined,
-        error: true,
-      });
-    }
-    const newExchangeRate = data.pop();
-    if (!newExchangeRate) {
-      return res.status(404).json({
-        message: 'Theres no exchange rate.',
-        data: undefined,
-        error: true,
-      });
-    }
-    await Settings.findByIdAndUpdate(
-      { _id: settings[0]._id },
-      { exchangeRate: { value: newExchangeRate.v, date: newExchangeRate.d } },
-      { new: true },
-    );
+  if (lastExchangeRate.date === yesterday.toISOString().substring(0, 10)) {
     return res.status(200).json({
       message: 'Showing exchange rate.',
-      data: { value: newExchangeRate.v, date: newExchangeRate.d },
+      data: lastExchangeRate,
       error: false,
     });
-  } catch (error: any) {
-    return res.status(400).json({
-      message: `Something went wrong: ${error.message}`,
-      data: undefined,
-      error: true,
-    });
   }
+  const response = await fetch('https://api.estadisticasbcra.com/usd_of', {
+    headers: {
+      Authorization: process.env.TOKEN_BCRA || '',
+    },
+  });
+  const data: ExchangeRate[] = await response.json();
+
+  if (!data) {
+    throw new CustomError(404, 'Something went wrong.');
+  }
+  const newExchangeRate = data.pop();
+  if (!newExchangeRate) {
+    throw new CustomError(404, 'Theres no exchange rate.');
+  }
+  await Settings.findByIdAndUpdate(
+    { _id: settings[0]._id },
+    { exchangeRate: { value: newExchangeRate.v, date: newExchangeRate.d } },
+    { new: true },
+  );
+  return res.status(200).json({
+    message: 'Showing exchange rate.',
+    data: { value: newExchangeRate.v, date: newExchangeRate.d },
+    error: false,
+  });
 };
