004946168945ebdb4feef15ac7718f385326ea46
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2022-12-14 12:00:00 -0300

    QL-247: implement mongoose transactions

--- src/routes/client/order/controllers.ts
+++ src/routes/client/order/controllers.ts
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import { Request, Response } from 'express';
+import { startSession } from 'mongoose';
 
 import { calculateAmounts, checkStock } from 'src/helper/orders';
 import s3 from 'src/helper/s3';
@@ -37,51 +38,61 @@ export const getOrderById = async (req: Request, res: Response) => {
 };
 
 export const createOrder = async (req: Request, res: Response) => {
-  const client = await Client.findOne({ _id: req.body.client, logicDelete: false });
-  if (!client) {
-    throw new CustomError(404, `Could not find a client by the id of ${req.params.id}.`);
-  }
-  if (!checkStock(req.body.products)) {
-    throw new CustomError(500, 'There is no stock left.');
-  }
-  if (!calculateAmounts(req.body.amounts, req.body.products, req.body.exchangeRate)) {
-    throw new CustomError(500, 'There has been an error during price calculation.');
-  }
+  const session = await startSession();
+  session.startTransaction();
+  try {
+    const client = await Client.findOne({ _id: req.body.client, logicDelete: false });
+    if (!client) {
+      throw new CustomError(404, `Could not find a client by the id of ${req.params.id}.`);
+    }
+    if (!checkStock(req.body.products)) {
+      throw new CustomError(500, 'There is no stock left.');
+    }
+    if (!calculateAmounts(req.body.amounts, req.body.products, req.body.exchangeRate)) {
+      throw new CustomError(500, 'There has been an error during price calculation.');
+    }
 
-  const payment = req.body.payment;
-  const uploadPayment = await s3.uploadFile(
-    payment,
-    process.env.AWS_BUCKET_TRANSFER_RECEIPTS || '',
-  );
-  const paymentFile = {
-    key: uploadPayment.Key,
-    url: uploadPayment.Location,
-  };
+    const payment = req.body.payment;
+    const uploadPayment = await s3.uploadFile(
+      payment,
+      process.env.AWS_BUCKET_TRANSFER_RECEIPTS || '',
+    );
+    const paymentFile = {
+      key: uploadPayment.Key,
+      url: uploadPayment.Location,
+    };
 
-  const newOrder = new Order({
-    ...req.body,
-    orderDate: format(new Date(), 'MM/dd/yyyy'),
-    payment: paymentFile,
-  });
-
-  const result = await newOrder.save();
-  if (result) {
-    req.body.products.forEach(async (product: OrderProduct) => {
-      const productUpdate = await Product.findOneAndUpdate(
-        { _id: product.product._id, logicDelete: false },
-        { stock: product.product.stock - product.quantity },
-        { new: true },
-      ).populate('category');
-      if (!productUpdate) {
-        throw new CustomError(500, 'Could not update the product stock.');
-      }
+    const newOrder = new Order({
+      ...req.body,
+      orderDate: format(new Date(), 'MM/dd/yyyy'),
+      payment: paymentFile,
     });
-  } else {
-    throw new CustomError(500, 'Could not create the order.');
+
+    const result = await newOrder.save({ session });
+    if (result) {
+      req.body.products.forEach(async (product: OrderProduct) => {
+        const productUpdate = await Product.findOneAndUpdate(
+          { _id: product.product._id, logicDelete: false },
+          { stock: product.product.stock - product.quantity },
+          { new: true },
+        ).populate('category');
+        if (!productUpdate) {
+          throw new CustomError(500, 'Could not update the product stock.');
+        }
+      });
+    } else {
+      throw new CustomError(500, 'Could not create the order.');
+    }
+    await session.commitTransaction();
+    return res.status(201).json({
+      message: 'Order created successfully.',
+      data: result,
+      error: false,
+    });
+  } catch (error: any) {
+    await session.abortTransaction();
+    throw new CustomError(error.status, error.message);
+  } finally {
+    session.endSession();
   }
-  return res.status(201).json({
-    message: 'Order created successfully.',
-    data: result,
-    error: false,
-  });
 };
