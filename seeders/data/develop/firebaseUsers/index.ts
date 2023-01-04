3284fb990f5bb654587cb0ee8623dbe9d7b712a0
Gina Schiappa Pietra <gina.schiappapietra@radiumrocket.com>
2023-01-04 18:25:48 -0300

    QL-377: update seeders

--- seeders/data/develop/firebaseUsers/index.ts
+++ seeders/data/develop/firebaseUsers/index.ts
@@ -3,84 +3,154 @@ import { FirebaseUser } from '../../../types';
 
 const firebaseAdmins: FirebaseUser[] = [
   {
-    email: 'admin1@qira.com',
-    password: 'test1234',
+    email: 'baltazar.cortina@qira.com',
+    password: 'Admin1234',
     uid: '4GFNFYixDNQmHG3Ia0m5EpVCkAz2',
     role: Role.ADMIN,
   },
   {
-    email: 'admin2@qira.com',
-    password: 'test1234',
+    email: 'ariana.maldonado@qira.com',
+    password: 'Admin1234',
     uid: 'IyhOppxNvqYXKClRfx3xRAXYCqu2',
     role: Role.ADMIN,
   },
   {
-    email: 'admin3@qira.com',
-    password: 'test1234',
+    email: 'gina.s.pietra@qira.com',
+    password: 'Admin1234',
     uid: 'GV713ckcZqXiiijzwEt4OI3ibbG3',
     role: Role.ADMIN,
   },
   {
-    email: 'admin4@qira.com',
-    password: 'test1234',
+    email: 'ivan.gobbi@qira.com',
+    password: 'Admin1234',
     uid: 'cMIQlyeZNZTD5N71psdLrQ8fV1n2',
     role: Role.ADMIN,
   },
   {
-    email: 'admin5@qira.com',
-    password: 'test1234',
-    uid: 'sFNhTBgdpgSSeFpoI6p5Teo6Jbo2',
+    email: 'francisco.gutierrez@qira.com',
+    password: 'Admin1234',
+    uid: 'LE9bKBs6kPbF2eF3v5fLyQ0wPzr2',
     role: Role.ADMIN,
   },
   {
-    email: 'admin6@qira.com',
-    password: 'test1234',
-    uid: 'LE9bKBs6kPbF2eF3v5fLyQ0wPzr2',
+    email: 'sabrina.taselli@qira.com',
+    password: 'Admin1234',
+    uid: 'sFNhTBgdpgSSeFpoI6p5Teo6Jbo2',
     role: Role.ADMIN,
   },
 ];
 
 const firebaseClients: FirebaseUser[] = [
+  // ENABLED
   {
-    email: 'qira@radiumrocket.com',
+    email: 'anabel.laurence@gmail.com',
     password: 'test1234',
     uid: 'Zk5wn4nxJMdeOy2tt3dZVRutCF22',
     role: Role.CLIENT,
   },
   {
-    email: 'mcdonalds@gmail.com',
+    email: 'leandro.techos@gmail.com',
     password: 'test1234',
     uid: 'zxwbzwhEhHWtcjPKtPJZadKaubR2',
     role: Role.CLIENT,
   },
   {
-    email: 'juanvenado@gmail.com',
+    email: 'cesia.maldonado@gmail.com',
     password: 'test1234',
     uid: 'IzvWs2rVsyeq1LFFSwZCiUxBI593',
     role: Role.CLIENT,
   },
   {
-    email: 'campoverde@gmail.com',
+    email: 'jose.blois@gmail.com',
     password: 'test1234',
     uid: 'tBTcjSX0OSQuIctZ4aoV5wGQj5f1',
     role: Role.CLIENT,
   },
   {
-    email: 'gina.cliente@gmail.com',
+    email: 'esteban.frare@gmail.com',
     password: 'test1234',
     uid: 'RYaNC1eVC8Yw2N1yfcPu5yDBxHA2',
     role: Role.CLIENT,
   },
   {
-    email: 'santomonte@gmail.com',
+    email: 'samuel.trillo@gmail.com',
     password: 'test1234',
     uid: 'qn8KW4DgFtbTtrCbmcn9zsBIDw73',
     role: Role.CLIENT,
   },
+  //DISAPPROVED
   {
-    email: 'tecnoagro@hotmail.com',
+    email: 'karen.soto@gmail.com',
     password: 'test1234',
     uid: 'fmhF69Km2dNL2pXCysrDELR7zrd2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'luciano.alarcon@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrd2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'juancruz.moreira@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrp1',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'axel.galindo@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrl2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'paula.rinaldi@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrm2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'nicolas.lobos@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrn2',
+    role: Role.CLIENT,
+  },
+
+  //DISABLED
+  {
+    email: 'franco.marini@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrv2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'iara.criscenti@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrv2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'agus.chazarreta@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrwz1',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'julian.demeglio@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrwc2',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'facundo.cosetino@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zrei',
+    role: Role.CLIENT,
+  },
+  {
+    email: 'iara.criscenti@gmail.com',
+    password: 'test1234',
+    uid: 'fmhF69Km2dNL2pXCysrDELR7zre2',
     role: Role.CLIENT,
   },
 ];
--- seeders/data/develop/orders/index.ts
+++ seeders/data/develop/orders/index.ts
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 // TODO: Fix IOrder type and change any -> IOrder
 const orders: any[] = [
   {
-    _id: '790IIUO4',
+    _id: '636534E7',
     products: [
       {
         product: new mongoose.Types.ObjectId('636534e7b1c86f2d04240078'),
-        quantity: 2,
+        quantity: 1,
       },
     ],
     client: new mongoose.Types.ObjectId('62891944b389642a7f13ca01'),
@@ -26,13 +26,13 @@ const orders: any[] = [
       },
     ],
     payment: {
-      key: 'develop/transfer-receipts/example-transfer.pdf',
-      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/example-transfer.pdf',
+      key: 'develop/transfer-receipts/Client-Payment-receipt-xample-transfer.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/Client-Payment-receipt-xample-transfer..pdf',
     },
     amounts: {
-      products: 640000,
-      taxes: 134400,
-      total: 774400,
+      products: 2000,
+      taxes: 420,
+      total: 2420,
     },
     state: 'APPROVE_PENDING',
     exchangeRate: 160,
@@ -40,7 +40,7 @@ const orders: any[] = [
     estimatedDeliveryDate: '10-20-2022',
   },
   {
-    _id: '3PK2AW0D',
+    _id: '12E45T78',
     products: [
       {
         product: new mongoose.Types.ObjectId('636534e7b1c86f2d04240078'),
@@ -66,12 +66,98 @@ const orders: any[] = [
       key: 'develop/transfer-receipts/example-transfer.pdf',
       url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/example-transfer.pdf',
     },
-    amounts: {
-      products: 320000,
-      taxes: 67200,
-      total: 387200,
+    invoice: {
+      key: 'develop/transfer-receipts/Approve-an-order-Factura.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/Approve-an-order-Factura.pdf',
     },
-    state: 'APPROVE_PENDING',
+    amounts: {
+      products: 2000,
+      taxes: 420,
+      total: 2420,
+    },
+    state: 'DELIVERY_PENDING',
+    exchangeRate: 160,
+    orderDate: '10-10-2022',
+    estimatedDeliveryDate: '10-20-2022',
+  },
+  {
+    _id: '636534E8',
+    products: [
+      {
+        product: new mongoose.Types.ObjectId('636534e7b1c86f2d04240078'),
+        quantity: 1,
+      },
+    ],
+    client: new mongoose.Types.ObjectId('62891944b389642a7f13ca01'),
+    authorized: [
+      {
+        firstName: 'Julian',
+        lastName: 'Parker',
+        dni: '42331778',
+        phoneNumber: '3416133021',
+      },
+      {
+        firstName: 'Lionel',
+        lastName: 'Mesias',
+        dni: '33456123',
+        phoneNumber: '3416133037',
+      },
+    ],
+    payment: {
+      key: 'develop/transfer-receipts/example-transfer.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/example-transfer.pdf',
+    },
+    invoice: {
+      key: 'develop/transfer-receipts/Approve-an-order-Factura.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/Approve-an-order-Factura.pdf',
+    },
+    signedInvoice: {
+      key: 'develop/transfer-receipts/Deliver-an-order-Factura-firmada.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/Deliver-an-order - Factura-firmada.pdf',
+    },
+    amounts: {
+      products: 2000,
+      taxes: 420,
+      total: 2420,
+    },
+    state: 'DELIVERED',
+    exchangeRate: 160,
+    orderDate: '10-10-2022',
+    estimatedDeliveryDate: '10-20-2022',
+  },
+  {
+    _id: '636534E9',
+    products: [
+      {
+        product: new mongoose.Types.ObjectId('636534e7b1c86f2d04240078'),
+        quantity: 1,
+      },
+    ],
+    client: new mongoose.Types.ObjectId('62891944b389642a7f13ca01'),
+    authorized: [
+      {
+        firstName: 'Julian',
+        lastName: 'Parker',
+        dni: '42331778',
+        phoneNumber: '3416133021',
+      },
+      {
+        firstName: 'Lionel',
+        lastName: 'Mesias',
+        dni: '33456123',
+        phoneNumber: '3416133037',
+      },
+    ],
+    payment: {
+      key: 'develop/transfer-receipts/Client-Payment-receipt-xample-transfer.pdf',
+      url: 'https://qira-local.s3.amazonaws.com/develop/transfer-receipts/Client-Payment-receipt-xample-transfer..pdf',
+    },
+    amounts: {
+      products: 2000,
+      taxes: 420,
+      total: 2420,
+    },
+    state: 'REJECTED',
     exchangeRate: 160,
     orderDate: '10-10-2022',
     estimatedDeliveryDate: '10-20-2022',
